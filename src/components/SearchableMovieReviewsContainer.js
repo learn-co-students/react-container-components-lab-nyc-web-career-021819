dd2efa291fbbe5ade54bb1262629a0da41ec5130
tchang9 <tlchang@gmail.com>
2019-04-28 09:22:21 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,47 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
-
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.getData()
+    }
+
+    getData = () => {
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}&api-key=A8dCpQWfufZZawDhPqMlVUO0IxqlPkqN`)
+        .then(res => res.json())
+        .then(reviews => {
+            this.setState({
+                reviews: reviews.results
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit} >
+                    <input onChange={this.handleChange}>
+                    </input>
+                    <button> Search Movie Reviews
+                    </button>
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
---
+++ src/components/data.js
@@ -0,0 +1,449 @@
+const data = {
+    status: "OK",
+    copyright: "Copyright (c) 2019 The New York Times Company. All Rights Reserved.",
+    has_more: true,
+    num_results: 20,
+    results: [
+        {
+            display_title: "Be Natural: The Untold Story of Alice Guy-Blaché",
+            mpaa_rating: "",
+            critics_pick: 1,
+            byline: "A.O. SCOTT",
+            headline: "‘Be Natural’ Review: Rescuing Alice Guy Blaché, a Film Pioneer, From Oblivion",
+            summary_short: "This lively documentary shows how a founder of two national cinemas — director, producer and studio boss — was almost forgotten.",
+            publication_date: "2019-04-25",
+            opening_date: "2018-12-07",
+            date_updated: "2019-04-28 02:44:30",
+            link: {
+            type: "article",
+            url: "http://www.nytimes.com/2019/04/25/movies/be-natural-untold-story-alice-guy-blache-review.html",
+            suggested_link_text: "Read the New York Times Review of Be Natural: The Untold Story of Alice Guy-Blaché"
+            },
+            multimedia: {
+            type: "mediumThreeByTwo210",
+            src: "https://static01.nyt.com/images/2019/04/25/arts/25benatural2/25benatural2-mediumThreeByTwo210.jpg",
+            width: 210,
+            height: 140
+            }
+        },
+    {
+    display_title: "If the Dancer Dances",
+    mpaa_rating: "",
+    critics_pick: 1,
+    byline: "BEN KENIGSBERG",
+    headline: "‘If the Dancer Dances’ Review: An Inheritance in Motion",
+    summary_short: "Maia Wechsler’s documentary goes backstage with dancers in a revival of Merce Cunningham’s “RainForest” as they learn to move from those who knew the groundbreaking choreographer.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-04-26",
+    date_updated: "2019-04-28 02:44:29",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/if-the-dancer-dances-review.html",
+    suggested_link_text: "Read the New York Times Review of If the Dancer Dances"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/26/arts/25ifthedancer1/25ifthedancer1-mediumThreeByTwo210-v2.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Queen of Diamonds",
+    mpaa_rating: "",
+    critics_pick: 1,
+    byline: "GLENN KENNY",
+    headline: "‘Queen of Diamonds’ Review: Fear, Loathing and Misogyny",
+    summary_short: "Nina Menkes’s 1991 film, debuting in a new restoration, keeps a tight frame on the accumulating indignities of capitalism in Las Vegas.",
+    publication_date: "2019-04-25",
+    opening_date: null,
+    date_updated: "2019-04-28 02:44:28",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/queen-of-diamonds-review.html",
+    suggested_link_text: "Read the New York Times Review of Queen of Diamonds"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/25/arts/25queenof/25queenof-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "JT LeRoy",
+    mpaa_rating: "R",
+    critics_pick: 0,
+    byline: "MANOHLA DARGIS",
+    headline: "‘J.T. LeRoy’ Review: Unpeeling the Layers of an All-American Hoax",
+    summary_short: "The well-matched Kristen Stewart and Laura Dern play creation and creator in this entertaining account of a literary fraud.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-04-26",
+    date_updated: "2019-04-25 17:30:03",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/jt-leroy-review.html",
+    suggested_link_text: "Read the New York Times Review of JT LeRoy"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/26/arts/jtleroy-1/merlin_153742809_b966ac27-6012-4770-a7e8-297ff6c4e163-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Thriller",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "JASON BAILEY",
+    headline: "‘Thriller’ Review: High School Slasher Offers Nothing New",
+    summary_short: "The diversity of its cast is refreshing, but the rest is a haphazard assemblage of recycled parts from other, better horror movies.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-04-14",
+    date_updated: "2019-04-25 16:32:02",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/thriller-review.html",
+    suggested_link_text: "Read the New York Times Review of Thriller"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/26/arts/25thriller/25thriller-mediumThreeByTwo210-v2.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "The White Crow",
+    mpaa_rating: "R",
+    critics_pick: 0,
+    byline: "MANOHLA DARGIS",
+    headline: "‘The White Crow’ Review: The Young Nureyev, Gotta Dance! Gotta Defect!",
+    summary_short: "Ralph Fiennes directs this biographical look at the ballet dancer Rudolf Nureyev’s early life, including his 1961 defection to the West.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-04-26",
+    date_updated: "2019-04-28 02:44:30",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/the-white-crow-review.html",
+    suggested_link_text: "Read the New York Times Review of The White Crow"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/26/arts/00thewhitecrow-print/merlin_153742704_96e956fe-814e-40f9-927b-b2bd09f425be-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "I Trapped the Devil",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "TEO BUGBEE",
+    headline: "‘I Trapped the Devil’ Review: Anxiety, Panic and Maybe Satan on a Holiday Visit",
+    summary_short: "Someone is being held in the basement in this stylish film.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-04-26",
+    date_updated: "2019-04-28 02:44:30",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/i-trapped-the-devil-review.html",
+    suggested_link_text: "Read the New York Times Review of I Trapped the Devil"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/25/arts/25itrapped/25itrapped-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Chasing Portraits",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "KEN JAWOROWSKI",
+    headline: "‘Chasing Portraits’ Review: Seeking Art Lost During the Holocaust",
+    summary_short: "The documentary follows Elizabeth Rynecki as she catalogs paintings by Moshe Rynecki that were scattered during World War II.",
+    publication_date: "2019-04-25",
+    opening_date: "2019-02-26",
+    date_updated: "2019-04-28 02:44:29",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/chasing-portraits-review.html",
+    suggested_link_text: "Read the New York Times Review of Chasing Portraits"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/25/arts/25chasing1/merlin_153742545_ffa37ea9-a84a-4208-8c88-7d570821ce12-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Return to Return to Nuke 'Em High Aka Vol. 2",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "GLENN KENNY",
+    headline: "‘Return to Return to Nuke ’Em High A.K.A. Vol. 2’ Review: Troma Is Back, Icky as Ever",
+    summary_short: "A putrid but at times oddly amiable exercise from the director Lloyd Kaufman.",
+    publication_date: "2019-04-25",
+    opening_date: null,
+    date_updated: "2019-04-28 02:44:28",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/return-to-return-to-nuke-em-high-review.html",
+    suggested_link_text: "Read the New York Times Review of Return to Return to Nuke 'Em High Aka Vol. 2"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/23/movies/return1/return1-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Tesla Nation",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "BEN KENIGSBERG",
+    headline: "‘Tesla Nation’ Review: A Documentary Celebrates Serbian-American Accomplishments",
+    summary_short: "It’s not surprising that there have been great Serbian contributions to the United States. But to whom is this documentary being addressed?",
+    publication_date: "2019-04-25",
+    opening_date: null,
+    date_updated: "2019-04-28 02:44:28",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/25/movies/tesla-nation-review.html",
+    suggested_link_text: "Read the New York Times Review of Tesla Nation"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/24/arts/tesla1/tesla1-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Carmine Street Guitars",
+    mpaa_rating: "",
+    critics_pick: 1,
+    byline: "GLENN KENNY",
+    headline: "‘Carmine Street Guitars’ Review: A Portrait of an Ax Meister",
+    summary_short: "The Greenwich Village guitar shop at the heart of Ron Mann’s documentary is one the last redoubts of a fast-disappearing bohemian Manhattan.",
+    publication_date: "2019-04-23",
+    opening_date: "2019-04-24",
+    date_updated: "2019-04-25 16:44:01",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/23/movies/carmine-street-guitars-review.html",
+    suggested_link_text: "Read the New York Times Review of Carmine Street Guitars"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/24/arts/23carmine-print/23carmine1-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Avengers: Endgame",
+    mpaa_rating: "PG-13",
+    critics_pick: 0,
+    byline: "A.O. SCOTT",
+    headline: "‘Avengers: Endgame’ Review: The Real Heroes Were the Friends We Made Along the Way",
+    summary_short: "Iron Man, Thor, the Hulk, Captain America and other stars settle scores, tie up loose ends and take a long victory lap in the final chapter of this cycle.",
+    publication_date: "2019-04-23",
+    opening_date: "2019-04-26",
+    date_updated: "2019-04-28 02:44:27",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/23/movies/avengers-endgame-review.html",
+    suggested_link_text: "Read the New York Times Review of Avengers: Endgame"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/23/arts/23avengers-hp-promo/23avengers-hp-promo-mediumThreeByTwo210-v5.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Kalank",
+    mpaa_rating: "Not Rated",
+    critics_pick: 0,
+    byline: "RACHEL SALTZ",
+    headline: "‘Kalank’ Review: A Bollywood Love Story in the Shadow of Partition",
+    summary_short: "Abhishek Varman’s maximalist melodrama delivers spectacle (some of it mind-numbing) but fails to tell a gripping story.",
+    publication_date: "2019-04-19",
+    opening_date: "2019-04-17",
+    date_updated: "2019-04-28 02:44:26",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/19/movies/kalank-review.html",
+    suggested_link_text: "Read the New York Times Review of Kalank"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/20/arts/00kalank-1/merlin_153438399_7f29e029-c5ed-4c59-84dd-91bf61373d59-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Fast Color",
+    mpaa_rating: "PG-13",
+    critics_pick: 1,
+    byline: "JEANNETTE CATSOULIS",
+    headline: "‘Fast Color’ Review: Can a Gifted Family Save a Parched World?",
+    summary_short: "Julia Hart’s wonderfully strange dystopian drama pits three gifted black women against a dried-up world and a hostile government.",
+    publication_date: "2019-04-18",
+    opening_date: "2019-04-19",
+    date_updated: "2019-04-26 16:44:02",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/fast-color-review.html",
+    suggested_link_text: "Read the New York Times Review of Fast Color"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/19/arts/fastcolor2/fastcolor2-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Grass",
+    mpaa_rating: "",
+    critics_pick: 1,
+    byline: "GLENN KENNY",
+    headline: "‘Grass’ Review: Eavesdropping in the Coffee Shop With Hong Sang-soo",
+    summary_short: "A sense of dislocation hangs over the South Korean director’s latest feature as a young woman writer observes mostly melancholy people, old and young.",
+    publication_date: "2019-04-18",
+    opening_date: null,
+    date_updated: "2019-04-18 11:04:07",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/grass-review.html",
+    suggested_link_text: "Read the New York Times Review of Grass"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/19/arts/18grass1/18grass1-mediumThreeByTwo210-v2.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "The Curse of La Llorona",
+    mpaa_rating: "R",
+    critics_pick: 0,
+    byline: "MANOHLA DARGIS",
+    headline: "‘The Curse of La Llorona’ Review: California Freakin’, With Scares, Laughs and a Murderous Ghost",
+    summary_short: "Linda Cardellini plays a widow with two children and a curse in this amusing, old-fashioned ghost story, the latest addition to the “Conjuring” series.",
+    publication_date: "2019-04-18",
+    opening_date: "2019-04-19",
+    date_updated: "2019-04-28 02:44:26",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/the-curse-of-la-llorona-review.html",
+    suggested_link_text: "Read the New York Times Review of The Curse of La Llorona"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/19/arts/00thecurse-1/00thecurse-1-mediumThreeByTwo210-v2.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Someone Great",
+    mpaa_rating: "R",
+    critics_pick: 0,
+    byline: "KAREN HAN",
+    headline: "‘Someone Great’ Review: After a Breakup, One Last Night on the Town",
+    summary_short: "Netflix’s contribution to the “girls’ night out” genre has its clichés, but the cast has ample chemistry to pull it off.",
+    publication_date: "2019-04-18",
+    opening_date: "2019-04-19",
+    date_updated: "2019-04-18 17:48:03",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/someone-great-review.html",
+    suggested_link_text: "Read the New York Times Review of Someone Great"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/18/arts/18somethinggreat/18somethinggreat-mediumThreeByTwo210-v2.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "The Man Who Killed Don Quixote",
+    mpaa_rating: "Not Rated",
+    critics_pick: 0,
+    byline: "A.O. SCOTT",
+    headline: "Review: ‘The Man Who Killed Don Quixote’ Brings Him Back to Life",
+    summary_short: "Terry Gilliam’s long-deferred tribute to Cervantes’s knight errant finally arrives, starring Jonathan Pryce and Adam Driver.",
+    publication_date: "2019-04-18",
+    opening_date: null,
+    date_updated: "2019-04-26 16:44:02",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/man-who-killed-don-quixote-review.html",
+    suggested_link_text: "Read the New York Times Review of The Man Who Killed Don Quixote"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/19/arts/18manwho1/merlin_153439065_4f784f3f-bef0-44dd-89c0-3c8f7a820f27-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Naples in Veils",
+    mpaa_rating: "",
+    critics_pick: 0,
+    byline: "BEN KENIGSBERG",
+    headline: "‘Naples in Veils’: A Morgue Doctor Is Obsessed. The Movie Is Lifeless.",
+    summary_short: "Ferzan Ozpetek’s thriller tips its hat to the motifs of Hitchcock, only to wind up as a tedious muddle.",
+    publication_date: "2019-04-18",
+    opening_date: "2019-04-19",
+    date_updated: "2019-04-26 16:44:02",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/naples-in-veils-review.html",
+    suggested_link_text: "Read the New York Times Review of Naples in Veils"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/18/arts/naples1/naples1-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    },
+    {
+    display_title: "Little Woods",
+    mpaa_rating: "R",
+    critics_pick: 0,
+    byline: "MANOHLA DARGIS",
+    headline: "‘Little Woods’ Review: Life Is Thicker Than Blood",
+    summary_short: "Tessa Thompson holds the center in this drama about a former drug smuggler on probation who is forced to find a new life for herself and her sister.",
+    publication_date: "2019-04-18",
+    opening_date: "2019-04-19",
+    date_updated: "2019-04-28 02:44:24",
+    link: {
+    type: "article",
+    url: "http://www.nytimes.com/2019/04/18/movies/little-woods-review.html",
+    suggested_link_text: "Read the New York Times Review of Little Woods"
+    },
+    multimedia: {
+    type: "mediumThreeByTwo210",
+    src: "https://static01.nyt.com/images/2019/04/19/arts/00littlewoods-1/00littlewoods-1-mediumThreeByTwo210.jpg",
+    width: 210,
+    height: 140
+    }
+    }
+    ]
+    }
+    export default data
